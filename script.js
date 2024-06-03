f89bf816859eb38166181d7882fe8b85fafa7108
sanket <sankrtpowar@gmail.com>
2024-06-02 23:54:48 -0700

    Calculator- JS , HTML , CSS Mini Project

---
+++ script.js
@@ -0,0 +1,28 @@
+let input = document.getElementById("inputbox");
+let buttons = document.querySelectorAll("button");
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(btn => {
+    btn.addEventListener("click", (e) => {  
+        
+        if (e.target.innerHTML == "=") {
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML=="AC"){
+            string="";
+            input.value = string;2
+        }
+        else if(e.target.innerHTML=="DLE"){
+            string=string.substring(0,string.length-1);
+            input.value = string;
+
+        }
+        else{
+            string+=e.target.innerHTML;
+            input.value = string;
+
+        }
+    })
+})
